c6e194ac57fff97bd4740da6029b783189724442
Antonio Marcos Goulart <antoniomarcos5674335@gmail.com>
2026-04-06 00:40:10 -0300

    feat: criação da AgendamentoNaoEncontradoExeption para o agendamento não encontrado usando o Id solicitado

---
+++ src/main/backend/com/antonio/SistemadeAgendamentodeConsultas/exception/AgendamentoNaoEncontradoExeption.java
@@ -0,0 +1,8 @@
+package com.antonio.SistemadeAgendamentodeConsultas.exception;
+
+public class AgendamentoNaoEncontradoExeption extends RuntimeException {
+    public AgendamentoNaoEncontradoExeption(String message) {
+        super(message);
+    }
+}
+
